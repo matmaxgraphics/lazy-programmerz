3dd80524cb6ae56692ef85dc80035f27b23846fe
MATEEN Gbadamosi <54564940+matmaxgraphics@users.noreply.github.com>
2021-08-09 06:53:24 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//Navbar Toggling
+function classToggle() {
+  const navs = document.querySelectorAll('.navbar__Items')
+  
+  navs.forEach(nav => nav.classList.toggle('navbar__ToggleShow'));
+}
+
+document.querySelector('.navbar__Link-toggle')
+  .addEventListener('click', classToggle);
